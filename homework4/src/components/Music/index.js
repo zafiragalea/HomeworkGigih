3cb6718bde5cd13633dc5879b480ee7599b4779e
zafiragalea <87572241+zafiragalea@users.noreply.github.com>
2022-03-26 13:55:24 +0700

    homeworkmodule2session03

--- homework4/src/components/Music/index.js
+++ homework4/src/components/Music/index.js
@@ -7,6 +7,7 @@ export default function Music({ url, name, artist, album, link }) {
       <h3>{name}</h3>
       <h3>{artist}</h3>
       <h5 style={{ color: "gray", fontWeight: 200 }}>{album}</h5>
+      
       <button id="select"><a href={link}>Select</a></button>
     </div>
   );
